dc56943cac7bcd87b3b69ca1950ddfcb0834ffad
Tomas <tomaasfer11@gmail.com>
2023-08-20 12:02:53 -0300

    remove lombok

--- src/main/java/com/app/appointment_app/disponibility/domain/model/Disponibility.java
+++ src/main/java/com/app/appointment_app/disponibility/domain/model/Disponibility.java
@@ -1,17 +1,53 @@
 package com.app.appointment_app.disponibility.domain.model;
 
 import com.app.appointment_app.doctor.domain.model.Doctor;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Disponibility {
     private Long idDisponibility;
     private Date hour;
     private Doctor doctor;
+
+    public Disponibility(Long idDisponibility, Date hour, Doctor doctor) {
+        this.idDisponibility = idDisponibility;
+        this.hour = hour;
+        this.doctor = doctor;
+    }
+
+    public Disponibility() {
+    }
+
+    public Long getIdDisponibility() {
+        return idDisponibility;
+    }
+
+    public void setIdDisponibility(Long idDisponibility) {
+        this.idDisponibility = idDisponibility;
+    }
+
+    public Date getHour() {
+        return hour;
+    }
+
+    public void setHour(Date hour) {
+        this.hour = hour;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    @Override
+    public String toString() {
+        return "Disponibility{" +
+                "idDisponibility=" + idDisponibility +
+                ", hour=" + hour +
+                ", doctor=" + doctor +
+                '}';
+    }
 }
