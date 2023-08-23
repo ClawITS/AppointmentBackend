fbd4acd6afe8acfe577a8656486125ce9e01a12d
Tomas <tomaasfer11@gmail.com>
2023-08-22 21:31:49 -0300

    first commit

--- src/main/java/com/app/appointment_app/doctor/infraestructure/mapper/DoctorMapper.java
+++ src/main/java/com/app/appointment_app/doctor/infraestructure/mapper/DoctorMapper.java
@@ -1,17 +1,9 @@
 package com.app.appointment_app.doctor.infraestructure.mapper;
-
-import com.app.appointment_app.disponibility.domain.model.Disponibility;
-import com.app.appointment_app.disponibility.infraestructure.mapper.DisponibilityMapper;
 import com.app.appointment_app.doctor.domain.model.Doctor;
 import com.app.appointment_app.doctor.infraestructure.driver_adapter.jpa_repository.DoctorData;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
 @NoArgsConstructor
 public class DoctorMapper {
