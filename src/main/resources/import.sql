e074815781a9da7b57f2796dfb4cf91e608eae80
Tomas <tomaasfer11@gmail.com>
2023-08-24 17:05:51 -0300

    import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO patient_data (name, document, birth_date, email, gender) VALUES
-  ('Nombre del paciente 1', 123456789, '1990-01-01', 'correo1@example.com', 'MALE');
-
-INSERT INTO patient_data (name, document, birth_date, email, gender) VALUES
-  ('Nombre del paciente 2', 987654321, '1995-05-10', 'correo2@example.com', 'FEMALE');
+INSERT INTO patients (birth_date, document, email, gender, name)
+VALUES
+  ('1990-01-01 00:00:00', 123456789, 'correo1@example.com', 0, 'Nombre del paciente 1'),
+  ('1995-05-10 00:00:00', 987654321, 'correo2@example.com', 0, 'Nombre del paciente 2'),
+  ('1988-11-25 00:00:00', 567890123, 'correo20@example.com', 1, 'Nombre del paciente 20');
