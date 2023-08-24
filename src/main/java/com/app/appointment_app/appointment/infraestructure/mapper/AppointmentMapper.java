00554856af17d60c55330ac520cb136c41fe6922
Tomas <tomaasfer11@gmail.com>
2023-08-24 15:30:06 -0300

    import

--- src/main/java/com/app/appointment_app/appointment/infraestructure/mapper/AppointmentMapper.java
+++ src/main/java/com/app/appointment_app/appointment/infraestructure/mapper/AppointmentMapper.java
@@ -2,12 +2,20 @@ package com.app.appointment_app.appointment.infraestructure.mapper;
 
 import com.app.appointment_app.appointment.domain.model.Appointment;
 import com.app.appointment_app.appointment.infraestructure.driver_adapter.jpa_repository.AppointmentData;
+import com.app.appointment_app.disponibility.domain.model.Disponibility;
+import com.app.appointment_app.doctor.domain.model.Doctor;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppointmentMapper {
     public Appointment toAppointment(AppointmentData appointmentData){
-        return new Appointment(appointmentData.getIdAppointment(),null,null,appointmentData.getState());
+        Disponibility disp = new Disponibility();
+        Doctor doctor = new Doctor();
+        doctor.setIdDoctor(appointmentData.getDisponibility().getDoctor().getIdDoctor());
+        doctor.setDescription(appointmentData.getDisponibility().getDoctor().getDescription());
+        disp.setDoctor(doctor);
+        disp.setHour(appointmentData.getDisponibility().getHour());
+        return new Appointment(appointmentData.getIdAppointment(),disp,null,appointmentData.getState());
     }
 
     public AppointmentData toData(Appointment appointment){
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO patient_data (name, document, birth_date, email, gender) VALUES
+  ('Nombre del paciente 1', 123456789, '1990-01-01', 'correo1@example.com', 'MALE');
+
+INSERT INTO patient_data (name, document, birth_date, email, gender) VALUES
+  ('Nombre del paciente 2', 987654321, '1995-05-10', 'correo2@example.com', 'FEMALE');
