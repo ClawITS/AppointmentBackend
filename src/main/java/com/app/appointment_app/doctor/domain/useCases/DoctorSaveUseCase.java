8f5c68b63907a9ccd8f25a5554eba33baa62a1b7
Tomas <tomaasfer11@gmail.com>
2023-08-24 19:33:54 -0300

    changes

--- src/main/java/com/app/appointment_app/doctor/domain/useCases/DoctorSaveUseCase.java
+++ src/main/java/com/app/appointment_app/doctor/domain/useCases/DoctorSaveUseCase.java
@@ -1,8 +1,12 @@
 package com.app.appointment_app.doctor.domain.useCases;
 
+import com.app.appointment_app.disponibility.domain.model.Disponibility;
+import com.app.appointment_app.disponibility.domain.model.enums.DisponibilityState;
 import com.app.appointment_app.doctor.domain.getways.DoctorDisponibilityRelationGetway;
 import com.app.appointment_app.doctor.domain.getways.DoctorSaveGetway;
 import com.app.appointment_app.doctor.domain.model.Doctor;
+
+import java.util.List;
 
 public class DoctorSaveUseCase {
     private final DoctorSaveGetway doctorSaveGetway;
@@ -17,7 +21,11 @@ public class DoctorSaveUseCase {
         if (!doctor.getDisponibilityList().isEmpty()) {
             if (doctor.getIdDoctor() == null) {
                 Doctor newDoctor = doctorSaveGetway.saveDoctor(doctor);
-                newDoctor.setDisponibilityList(doctor.getDisponibilityList());
+                List<Disponibility> disponibilityList = doctor.getDisponibilityList();
+                disponibilityList.forEach(
+                        disponibility -> disponibility.setDisponibilityState(DisponibilityState.AVAILABLE)
+                );
+                newDoctor.setDisponibilityList(disponibilityList);
                 return doctorDisponibilityRelation.disponibilityRelation(newDoctor);
             }
             return doctorDisponibilityRelation.disponibilityRelation(doctor);
