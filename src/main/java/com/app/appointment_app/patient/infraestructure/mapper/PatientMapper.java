488640c9a081fe2073a39bba9aca15dbe024c62d
Tomas <tomaasfer11@gmail.com>
2023-08-25 10:45:01 -0300

    checking relations

--- src/main/java/com/app/appointment_app/patient/infraestructure/mapper/PatientMapper.java
+++ src/main/java/com/app/appointment_app/patient/infraestructure/mapper/PatientMapper.java
@@ -1,18 +1,41 @@
 package com.app.appointment_app.patient.infraestructure.mapper;
 
+import com.app.appointment_app.appointment.domain.model.Appointment;
+import com.app.appointment_app.disponibility.domain.model.Disponibility;
+import com.app.appointment_app.disponibility.infraestructure.driver_adapter.jpa_repository.DisponibilityData;
+import com.app.appointment_app.doctor.domain.model.Doctor;
+import com.app.appointment_app.doctor.infraestructure.driver_adapter.jpa_repository.DoctorData;
 import com.app.appointment_app.patient.domain.model.Patient;
 import com.app.appointment_app.patient.infraestructure.driver_adapter.jpa_repository.PatientData;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class PatientMapper {
 
 
     public Patient toPatient(PatientData patientData) {
+        List<Appointment> appointmentList = patientData.getAppointmentDataList().stream().map(
+                appointmentData -> new Appointment(appointmentData.getIdAppointment(),
+                        disponibilityDataToDisponibility(appointmentData.getDisponibility()
+                ), new Patient(patientData.getIdPatient(),patientData.getName(),patientData.getDocument()
+                , patientData.getBirthDate(),patientData.getEmail(),patientData.getGender(),
+                        null), appointmentData.getState())
+        ).collect(Collectors.toList());
         return new Patient(patientData.getIdPatient(), patientData.getName(), patientData.getDocument(), patientData.getBirthDate(),
-                patientData.getEmail(), patientData.getGender(), null);
+                patientData.getEmail(), patientData.getGender(), appointmentList);
     }
 
+    public Disponibility disponibilityDataToDisponibility(DisponibilityData disponibilityData){
+        return new Disponibility(disponibilityData.getIdDisponibility(), disponibilityData.getHour(),
+                doctorDataToDoctor(disponibilityData.getDoctor()), disponibilityData.getDisponibilityState());
+    }
+    public Doctor doctorDataToDoctor(DoctorData doctorData){
+        return new Doctor(doctorData.getIdDoctor(),doctorData.getName(), doctorData.getDescription(),
+                null);
+    }
     public PatientData toData(Patient patient) {
         return new PatientData(patient.getIdPatient(), patient.getName(), patient.getDocument(), patient.getBirthDate(),
                 patient.getEmail(), patient.getGender(), null);
