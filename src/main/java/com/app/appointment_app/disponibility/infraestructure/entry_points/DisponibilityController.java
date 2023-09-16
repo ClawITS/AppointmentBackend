ddda105a95d1fc2d0493ed325897926acc40ee77
Tomas <tomaasfer11@gmail.com>
2023-09-15 23:06:37 -0300

    doctor and disponibility provider

--- src/main/java/com/app/appointment_app/disponibility/infraestructure/entry_points/DisponibilityController.java
+++ src/main/java/com/app/appointment_app/disponibility/infraestructure/entry_points/DisponibilityController.java
@@ -4,12 +4,7 @@ import com.app.appointment_app.disponibility.domain.model.Disponibility;
 import com.app.appointment_app.disponibility.domain.requests.DisponibilityFilterByDoctorAndHourRequest;
 import com.app.appointment_app.disponibility.domain.requests.DisponibilityFilterByHourRequest;
 import com.app.appointment_app.disponibility.domain.requests.DisponibilitySaveRequest;
-import com.app.appointment_app.disponibility.domain.usecases.filters.DisponibilityFilterByDoctorAndHourUseCase;
-import com.app.appointment_app.disponibility.domain.usecases.filters.DisponibilityFilterByHourUseCase;
-import com.app.appointment_app.disponibility.domain.usecases.cruds.DisponibilityDeleteUseCase;
-import com.app.appointment_app.disponibility.domain.usecases.cruds.DisponibilityFindAllUseCase;
-import com.app.appointment_app.disponibility.domain.usecases.cruds.DisponibilityFindByIdUseCase;
-import com.app.appointment_app.disponibility.domain.usecases.cruds.DisponibilitySaveUseCase;
+import com.app.appointment_app.disponibility.infraestructure.entry_points.provider.DisponibilityProvider;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,52 +15,48 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/disponibilities")
 public class DisponibilityController {
-    private final DisponibilitySaveUseCase disponibilitySaveUseCase;
-    private final DisponibilityDeleteUseCase disponibilityDeleteUseCase;
-    private final DisponibilityFindAllUseCase disponibilityFindAllUseCase;
-    private final DisponibilityFindByIdUseCase disponibilityFindByIdUseCase;
-    private final DisponibilityFilterByHourUseCase disponibilityFilterByHourUseCase;
-    private final DisponibilityFilterByDoctorAndHourUseCase disponibilityFilterByDoctorAndHourUseCase;
 
-    public DisponibilityController(DisponibilitySaveUseCase disponibilitySaveUseCase, DisponibilityDeleteUseCase
-            disponibilityDeleteUseCase, DisponibilityFindAllUseCase disponibilityFindAllUseCase, DisponibilityFindByIdUseCase disponibilityFindByIdUseCase, DisponibilityFilterByHourUseCase disponibilityFilterByHourUseCase, DisponibilityFilterByDoctorAndHourUseCase disponibilityFilterByDoctorAndHourUseCase) {
-        this.disponibilitySaveUseCase = disponibilitySaveUseCase;
-        this.disponibilityDeleteUseCase = disponibilityDeleteUseCase;
-        this.disponibilityFindAllUseCase = disponibilityFindAllUseCase;
-        this.disponibilityFindByIdUseCase = disponibilityFindByIdUseCase;
-        this.disponibilityFilterByHourUseCase = disponibilityFilterByHourUseCase;
-        this.disponibilityFilterByDoctorAndHourUseCase = disponibilityFilterByDoctorAndHourUseCase;
+    private final DisponibilityProvider disponibilityProvider;
+
+    public DisponibilityController(DisponibilityProvider disponibilityProvider) {
+        this.disponibilityProvider = disponibilityProvider;
     }
+
     @GetMapping("/page/{numberPage}")
     public ResponseEntity<Page<Disponibility>>findAllDisponibilities(@PathVariable int numberPage){
-        return new ResponseEntity<>(disponibilityFindAllUseCase.findAllDisponibilityPaginator(numberPage)
+        return new ResponseEntity<>(disponibilityProvider
+                .getDisponibilityFindAllUseCase().findAllDisponibilityPaginator(numberPage)
         , HttpStatus.OK);
     }
     @PostMapping
     public ResponseEntity<Disponibility>saveDisponibility(@RequestBody DisponibilitySaveRequest disponibilitySaveRequest){
-        return new ResponseEntity<>(disponibilitySaveUseCase.saveDisponibility(disponibilitySaveRequest),
+        return new ResponseEntity<>(disponibilityProvider
+                .getDisponibilitySaveUseCase().saveDisponibility(disponibilitySaveRequest),
                 HttpStatus.OK);
     }
     @GetMapping("/{id}")
     public ResponseEntity<Disponibility>findDisponibilityById(@PathVariable Long id){
-        return new ResponseEntity<>(disponibilityFindByIdUseCase.findDisponibilityById(id), HttpStatus.OK);
+        return new ResponseEntity<>(disponibilityProvider
+                .getDisponibilityFindByIdUseCase().findDisponibilityById(id), HttpStatus.OK);
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<String>deleteDisponibilityById(@PathVariable Long id){
-        disponibilityDeleteUseCase.deleteDisponibilityById(id);
+        disponibilityProvider.getDisponibilityDeleteUseCase().deleteDisponibilityById(id);
         return new ResponseEntity<>( "the disponibility with id "+ id +" has been deleted",
                 HttpStatus.OK);
     }
     @PostMapping("/filter-hour-range")
     public ResponseEntity<List<Disponibility>>filterDisponibilityByHourRange(@RequestBody DisponibilityFilterByHourRequest
                                                                              disponibilityFilterByHourRequest){
-        return new ResponseEntity<>(disponibilityFilterByHourUseCase.findDisponibilitiesByHour(disponibilityFilterByHourRequest)
+        return new ResponseEntity<>(disponibilityProvider
+                .getDisponibilityFilterByHourUseCase().findDisponibilitiesByHour(disponibilityFilterByHourRequest)
                 ,HttpStatus.OK);
     }
     @PostMapping("/filter-hour-range-doctor")
     public ResponseEntity<List<Disponibility>>filterDisponibilityByDoctorAndHourRange(@RequestBody DisponibilityFilterByDoctorAndHourRequest
                                                                                      disponibilityFilterByDoctorAndHourRequest){
-        return new ResponseEntity<>(disponibilityFilterByDoctorAndHourUseCase.filterDisponibilittyByDoctorAndHour(disponibilityFilterByDoctorAndHourRequest)
+        return new ResponseEntity<>(disponibilityProvider
+                .getDisponibilityFilterByDoctorAndHourUseCase().filterDisponibilittyByDoctorAndHour(disponibilityFilterByDoctorAndHourRequest)
                 ,HttpStatus.OK);
     }
 
--- src/main/java/com/app/appointment_app/doctor/infraestructure/entry_point/DoctorController.java
+++ src/main/java/com/app/appointment_app/doctor/infraestructure/entry_point/DoctorController.java
@@ -6,12 +6,7 @@ import com.app.appointment_app.doctor.domain.requests.AcceptPatientReschedulingR
 import com.app.appointment_app.doctor.domain.requests.RescheduleAppointmentRequest;
 import com.app.appointment_app.doctor.domain.responses.AcceptPatientReschedulingResponse;
 import com.app.appointment_app.doctor.domain.responses.RescheduleAppointmentResponse;
-import com.app.appointment_app.doctor.domain.useCases.business_services.AcceptPatientReschedulingUseCase;
-import com.app.appointment_app.doctor.domain.useCases.business_services.RescheduleAppointmentUseCase;
-import com.app.appointment_app.doctor.domain.useCases.cruds.DoctorDeleteUseCase;
-import com.app.appointment_app.doctor.domain.useCases.cruds.DoctorFindAllUseCase;
-import com.app.appointment_app.doctor.domain.useCases.cruds.DoctorFindByIdUseCase;
-import com.app.appointment_app.doctor.domain.useCases.cruds.DoctorSaveUseCase;
+import com.app.appointment_app.doctor.infraestructure.entry_point.provider.DoctorProvider;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,48 +15,43 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/doctors")
 public class DoctorController {
-    private final DoctorSaveUseCase doctorSaveUseCase;
-    private final DoctorDeleteUseCase doctorDeleteUseCase;
-    private final DoctorFindByIdUseCase doctorFindByIdUseCase;
-    private final DoctorFindAllUseCase doctorFindAllUseCase;
-    private final RescheduleAppointmentUseCase rescheduleAppointmentUseCase;
-    private final AcceptPatientReschedulingUseCase acceptPatientReschedulingUseCase;
+   private final DoctorProvider doctorProvider;
 
-    public DoctorController(DoctorSaveUseCase doctorSaveUseCase, DoctorDeleteUseCase doctorDeleteUseCase, DoctorFindByIdUseCase doctorFindByIdUseCase, DoctorFindAllUseCase doctorFindAllUseCase, RescheduleAppointmentUseCase rescheduleAppointmentUseCase, AcceptPatientReschedulingUseCase acceptPatientReschedulingUseCase) {
-        this.doctorSaveUseCase = doctorSaveUseCase;
-        this.doctorDeleteUseCase = doctorDeleteUseCase;
-        this.doctorFindByIdUseCase = doctorFindByIdUseCase;
-        this.doctorFindAllUseCase = doctorFindAllUseCase;
-        this.rescheduleAppointmentUseCase = rescheduleAppointmentUseCase;
-        this.acceptPatientReschedulingUseCase = acceptPatientReschedulingUseCase;
+    public DoctorController(DoctorProvider doctorProvider) {
+        this.doctorProvider = doctorProvider;
     }
+
 
     @PostMapping
     public ResponseEntity<Doctor> saveDoctor(@RequestBody Doctor doctor) {
-        return new ResponseEntity<>(doctorSaveUseCase.saveDoctor(doctor), HttpStatus.CREATED);
+        return new ResponseEntity<>(doctorProvider
+                .getDoctorSaveUseCase().saveDoctor(doctor), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteDoctorById(@PathVariable Long id) {
-        doctorDeleteUseCase.deleteDoctorById(id);
+        doctorProvider.getDoctorDeleteUseCase().deleteDoctorById(id);
         return new ResponseEntity<>("The doctor with id " + id + " has been deleted succesfuly", HttpStatus.OK);
 
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Doctor> findDoctorById(@PathVariable Long id) {
-        return new ResponseEntity<>(doctorFindByIdUseCase.findDoctorById(id), HttpStatus.OK);
+        return new ResponseEntity<>(doctorProvider
+                .getDoctorFindByIdUseCase().findDoctorById(id), HttpStatus.OK);
     }
 
     @GetMapping("/page/{numberPage}")
     public ResponseEntity<Page<Doctor>> findAllPageDoctors(@PathVariable int numberPage) {
-        return new ResponseEntity<>(doctorFindAllUseCase.findAllDoctorsPaginator(numberPage), HttpStatus.OK);
+        return new ResponseEntity<>(doctorProvider
+                .getDoctorFindAllUseCase().findAllDoctorsPaginator(numberPage), HttpStatus.OK);
     }
 
     @PostMapping("/rescheduleAppointment")
     public ResponseEntity<RescheduleAppointmentResponse> rescheduleAppointment(@RequestBody RescheduleAppointmentRequest
                                                                                        rescheduleAppointmentRequest) {
-        return new ResponseEntity<>(rescheduleAppointmentUseCase.rescheduleAppointment(rescheduleAppointmentRequest),
+        return new ResponseEntity<>(doctorProvider.getRescheduleAppointmentUseCase()
+                .rescheduleAppointment(rescheduleAppointmentRequest),
                 HttpStatus.OK);
     }
 
@@ -69,7 +59,8 @@ public class DoctorController {
     public ResponseEntity<AcceptPatientReschedulingResponse> acceptReschedule(@RequestBody AcceptPatientReschedulingRequest acceptPatientReschedulingRequest) {
         try{
             AcceptPatientRescheduleException.invalidState(acceptPatientReschedulingRequest);
-            return new ResponseEntity<>(acceptPatientReschedulingUseCase.acceptPatientRescheduling(acceptPatientReschedulingRequest),
+            return new ResponseEntity<>(doctorProvider
+                    .getAcceptPatientReschedulingUseCase().acceptPatientRescheduling(acceptPatientReschedulingRequest),
                     HttpStatus.OK);
         }catch(DoctorException e){
             return ResponseEntity.badRequest().body(new AcceptPatientReschedulingResponse(e.getMessage()));
